550c6883b1b1a7c7f8144c6ed706ae8da2135fc9
adarshzpatel <adarshzpatel@gmail.com>
2023-10-22 01:35:20 +0530

    uncomment

--- frontend/src/pages/repay.tsx
+++ frontend/src/pages/repay.tsx
@@ -95,21 +95,21 @@ const RepayPage = (props: Props) => {
     try {
       setIsProcessing(true);
 
-      // const approveDaiTx = await _approveDaiToken({
-      //   args: [STRATEGY_MANAGER_ADDRESS, selectedInvoice.amount],
-      // });
-      // const approveInvTx = await _approveInvoiceToken({
-      //   args: [INVOICE_FINANCER_ADDRESS, selectedInvoice.amount],
-      // });
-      // toast.success("Approved dai token");
+      const approveDaiTx = await _approveDaiToken({
+        args: [STRATEGY_MANAGER_ADDRESS, selectedInvoice.amount],
+      });
+      const approveInvTx = await _approveInvoiceToken({
+        args: [INVOICE_FINANCER_ADDRESS, selectedInvoice.amount],
+      });
+      toast.success("Approved dai token");
 
-      // await waitForConfirmation(10 * 1000);
-      // console.log(selectedInvoice.amount, stakedTokens);
-      // const repayTx = await _repay({
-      //   args: [DAI_ADDRESS, selectedInvoice.amount],
-      // });
+      await waitForConfirmation(10 * 1000);
+      console.log(selectedInvoice.amount, stakedTokens);
+      const repayTx = await _repay({
+        args: [DAI_ADDRESS, selectedInvoice.amount],
+      });
 
-      // await waitForConfirmation(10 * 1000);
+      await waitForConfirmation(10 * 1000);
 
       const paidInvoice = await _payInvoice({
         args: [selectedInvoice.invoiceId, selectedInvoice.amount],
