f2ee44f153ac9f0dd7cacd34c7f182a6ba51255d
adarshzpatel <adarshzpatel@gmail.com>
2023-10-22 10:50:42 +0530

    show repayment

--- frontend/src/components/borrow/InvoiceItem.tsx
+++ frontend/src/components/borrow/InvoiceItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { InvoiceType } from "../types/invoice.type";
 import {
   Button,
@@ -9,6 +9,19 @@ import {
   Divider,
 } from "@nextui-org/react";
 import { BsArrowRight } from "react-icons/bs";
+import { useContractRead } from "wagmi";
+import { INVOICE_FINANCER_ADDRESS } from "../constants/addresses";
+import { InvoiceFinancerABI } from "../constants/abi";
+
+enum InvoiceStatus {
+  None = 0,
+  Financed = 1,
+  Repaid = 2,
+}
+
+function getStatusString(statusNumber: number ): string {
+  return InvoiceStatus[statusNumber];
+}
 
 type Props = {
   invoice: InvoiceType;
@@ -17,6 +30,23 @@ type Props = {
 };
 
 const InvoiceItem = ({ invoice, handleMintClick, isMinted }: Props) => {
+  const [status,setStatus] = useState(invoice.status);
+  const colorMap = {
+    "Financed":"warning",
+    "Repaid":"success",
+  }
+  const {data} = useContractRead({
+    abi:InvoiceFinancerABI,
+    address:INVOICE_FINANCER_ADDRESS,
+    functionName:'invoices',
+    args:[invoice.id],
+    onSuccess(data:any) {
+      if(data){
+        setStatus(getStatusString(Number(data[2])))
+      }
+    },
+})
+
   return (
     <Card className={`bg-default-100/50 group  hover:ring-primary hover:ring-1 ${isMinted && "brightness-75 hover:ring-default-300"}`}>
       <CardBody>
@@ -24,21 +54,9 @@ const InvoiceItem = ({ invoice, handleMintClick, isMinted }: Props) => {
           <h6 className="font-semibold font-heading text-xl">
             {invoice.contract.title}
           </h6>
-          {isMinted && (
-            <Chip  color="primary">
-              Already Minted
-            </Chip>
-          )}
-          {!isMinted && invoice.status === "paid" && (
-            <Chip variant="flat" color="success">
-              Paid
-            </Chip>
-          )}
-          {!isMinted && invoice.status === "approved" && (
-            <Chip variant="flat" color="warning">
-              Approved
-            </Chip>
-          )}
+         {status !=='None' && <Chip color={colorMap[status]} className='capitalize'>
+            {status === 'None' ? invoice.status : status}
+          </Chip>}
         </div>
         <div className="text-default-500">
           <p className="capitalize ">
